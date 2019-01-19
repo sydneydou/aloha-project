b638d82c8cc1056f547d2bf4f4451bb906532c9e
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-01-18 16:22:40 -0800

    CSS specs

--- aloha.js
+++ aloha.js
@@ -1,39 +1,65 @@
-
 //email validation
 
-$(".sub").on("click", function(){  
+$(".sub").on("click", function() {
+  //http://www.jquerybyexample.net/2011/04/validate-email-address-using-jquery.html
 
-//http://www.jquerybyexample.net/2011/04/validate-email-address-using-jquery.html
-
-    function validateEmail(sEmail) {
-        var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-        console.log(filter.test(sEmail));
-        if (filter.test(sEmail)) {
-            return true;
-        } else {
-         return false;
-        }
+  function validateEmail(sEmail) {
+    var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    console.log(filter.test(sEmail));
+    if (filter.test(sEmail)) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    const sEmail= $(".mail").val();
-                               
-         if(validateEmail(sEmail)
-           ){
-            alert("Thanks for Subscribing!");
-           
-  }                   
-           else{
-             alert("Invalid Email");
-            
-           }
-             });
-  
+  const sEmail = $(".mail").val();
 
+  if (validateEmail(sEmail)) {
+    alert("Thanks for Subscribing!");
+  } else {
+    alert("Invalid Email");
+  }
+});
 
 //flickity carousel
 
-$('.product-all').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true
+$(".product-all").flickity({
+  // options
+  cellAlign: "left",
+  contain: true
+});
+
+// border click
+
+$(".product-all").on("focus", function() {
+  $(this).addClass("product-click");
+});
+
+$(".product-all").on("blur", function() {
+  $(this).removeClass("product-click");
+});
+
+//smooth scroll
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000
+        );
+        return false;
+      }
+    }
   });
+});
